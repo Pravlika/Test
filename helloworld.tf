b70ef9356bcc90d8923d982c9871069aabd1f588
Pravlika <pravalikarao239@gmail.com>
2017-06-28 15:51:01 -0400

    Update helloworld.tf

--- helloworld.tf
+++ helloworld.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   subscription_id = "1cd32318-ce1f-40a3-813a-8311793869c9"
   client_id       = "6598571a-02dd-4512-905a-46153a6d0339"
-  client_secret   = "jiZVJYpK4piswUSVUU+b75hJAjL5OqTcdEUXQFW/mEE="
+  client_secret   = "I+TrC17D3OZ16zgXMhrzU8GodhV909XITieIKGeTKi0="
   tenant_id       = "6bf32af2-2e48-49ca-801d-9335f08e95de"
 }
 
