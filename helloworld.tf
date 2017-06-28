e7d582b223e4a50766274fc48e4c04c85d404363
pravalika <pravalikayr7@gmail.com>
2017-06-28 13:27:54 -0400

    Adding modified helloworld script

--- helloworld.tf
+++ helloworld.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   subscription_id = "1cd32318-ce1f-40a3-813a-8311793869c9"
-  client_id       = "5ba8e2de-1722-4f1a-aade-59bf92bf0d3f"
+  client_id       = "eb61aa2d-dc7a-4570-a3de-ceebdf1e1b76"
   client_secret   = "6hKoX5jGz3arIsBRev3Hos0ETgZG4KJWgshxcLSlpBc="
   tenant_id       = "6bf32af2-2e48-49ca-801d-9335f08e95de"
 }
